811b9dbadfcfa11a2660b62d262aab591e11f2fc
lilbex <47287618+lilbex@users.noreply.github.com>
2021-06-22 16:27:41 +0100

    udate test

--- src/components/CartNotification/CartNotification.js
+++ src/components/CartNotification/CartNotification.js
@@ -3,12 +3,11 @@ import {MainContainer, BadgeContainer, Notification,Cart } from "./CartNofificat
 function CartNotification(props) {
   return (
     <MainContainer data-testid="button">
-      <Cart WIDTH="" src="https://res.cloudinary.com/devsprime/image/upload/v1624373777/Icons%20and%20Logo/cart_naqql0.png" alt="cart"/>
+      <Cart WIDTH="100px" src="https://res.cloudinary.com/devsprime/image/upload/v1624373777/Icons%20and%20Logo/cart_naqql0.png" alt="cart"/>
       <BadgeContainer backgroundColor="linear-gradient(253.58deg, #FFC000 1.55%, #FF8A00 95.8%)">
         <Notification>{props.number}</Notification>
       </BadgeContainer>
     </MainContainer>
   );
 }
-
 export default CartNotification;
