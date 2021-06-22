36b33e1353379458620243476f5a9400ca70b0b3
lilbex <47287618+lilbex@users.noreply.github.com>
2021-06-22 16:01:36 +0100

    add cloudinary

--- src/components/CartNotification/CartNotification.js
+++ src/components/CartNotification/CartNotification.js
@@ -1,10 +1,9 @@
-import cart from "./cart.png";
 import {MainContainer, BadgeContainer, Notification,Cart } from "./CartNofification.styles"
 
 function CartNotification(props) {
   return (
     <MainContainer data-testid="button">
-      <Cart WIDTH="" src={cart} alt="cart"/>
+      <Cart WIDTH="" src="https://res.cloudinary.com/devsprime/image/upload/v1624373777/Icons%20and%20Logo/cart_naqql0.png" alt="cart"/>
       <BadgeContainer backgroundColor="linear-gradient(253.58deg, #FFC000 1.55%, #FF8A00 95.8%)">
         <Notification>{props.number}</Notification>
       </BadgeContainer>
