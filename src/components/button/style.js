8cb3df34f382bb2428422f0170fa2084f412db29
Tosin Ayodele <devtosxn@gmail.com>
2021-06-24 09:49:24 +0100

    implement button component feature (#9)
    * implement button component feature
    
    * fix describe block in test file and add handleclick as props
    
    * change mediumWith to medium and largeWidth to large
    
    Co-authored-by: Decagon <decagon@Tosins-MacBook-Air.local>
    Co-authored-by: Olatunde Sodiq <olatundesodiq@gmail.com>

---
+++ src/components/button/style.js
@@ -0,0 +1,28 @@
+import styled, {css} from 'styled-components'
+
+const ButtonWrapper = styled.button`
+    color: #FFC000;
+    box-sizing:border-box;
+    background: "white";
+    padding: 12px 30px;
+    border-radius: 100px;
+    box-shadow: 0px 12px 35px rgba(160, 121, 0, 0.2);
+    border: none;
+    cursor: pointer;
+    ${({primary}) => primary && css`
+    color: #FFFFFF;
+    background:linear-gradient(253.58deg, #FFC000 1.55%, #FF8A00 95.8%);
+    `}
+    ${({medium}) => medium && css`
+    padding: 20px 65px;
+    `}
+    ${({large}) => large && css`
+    padding: 20px 115px;
+    `}
+    @media (max-width:768px) {
+    flex-direction: column;
+    transform: scale(0.8);
+  }
+`;
+
+export default ButtonWrapper;
