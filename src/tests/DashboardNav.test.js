6339dba0e82c001abece56c76d2c3f6af54d7b13
OMOLADE OLOGUN <81363219+Ladeologun@users.noreply.github.com>
2021-06-27 16:30:22 +0100

    Ft dashboard nav (#12)
    * implementation of dashboard nav component
    
    * ft-dashboardnav component implementation
    
    * ft-dashboard modification
    
    * ft-dashboardnav modification
    
    * ft-dashboard modification
    
    * ft-dashboardnav  refactor
    
    Co-authored-by: Adenike Awofeso <41590285+nikkifeso@users.noreply.github.com>

---
+++ src/tests/DashboardNav.test.js
@@ -0,0 +1,32 @@
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import DashBoardNav from '../components/DashboardNav/index'
+
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+describe('component rendering', () => {
+  test("component renders without props", () => {
+    act(() => {
+      render(<DashBoardNav />, container);
+    });
+    expect(container.textContent).toBe("");
+  });  
+});
+describe('<DasgboardNav />', () => {
+  test("renders with a props provided", () => {
+    act(() => {
+      render(<DashBoardNav details="Course Details" instructor="Teacher" date="Date" category="Category" type="Type" />, container);
+    });
+    expect(container.textContent).toBe("Course DetailsCategoryTypeTeacherDate");
+  });
+});
+
