36cafbd251207e87b194f94c2bbc7951a275cb14
ajibolagureje <49361680+ajibolagureje@users.noreply.github.com>
2021-06-27 23:21:34 +0100

    Searchbar Component Implementation (#28)
    * Searchbar Component Implementation
    
    * changed icon from prop to url
    
    Co-authored-by: Adenike Awofeso <41590285+nikkifeso@users.noreply.github.com>
    Co-authored-by: Adenike Awofeso <adenikeawofeso@gmail.com>

---
+++ src/components/search/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Section, Input, Hr, Icon} from './style'
+
+
+function Search(props) {
+  return (
+    <Section  id="search"
+      leftGradientColor={props.leftGradientColor}
+      rightGradientColor={props.rightGradientColor}
+    >
+      <Icon src="https://res.cloudinary.com/devsprime/image/upload/v1624378769/Icons%20and%20Logo/icons8_ofhg40.svg"></Icon>
+      <Input
+        leftGradientColor={props.leftGradientColor}
+        rightGradientColor={props.rightGradientColor}
+        type={props.type}
+        placeholder={props.placeholder}
+      />
+      <Hr />
+    </Section>
+  );
+}
+
+export default Search;
+
